8c57a59c722e2abcc46785210a5ba0bb8b4f0337
Uzenir3serieB <uzenir3serieB@gmail.com>
2024-09-18 23:00:16 -0300

    teste

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const avanca = document.querySelectorAll(".btn-proximo");
 avanca.forEach(button => {
     button.addEventListener("click",function(){
         const atual = document.querySelector(".ativo");
-        const proximopasso = "passo-" + this.getAttribute("data-proximo");
+        const proximoPasso = "passo-" + this.getAttribute("data-proximo");
 
         atual.classList.remove("ativo");
-        document.getElementById(proximopasso).classList.add("ativo");
+        document.getElementById(proximoPasso).classList.add("ativo");
 
     })
 })
